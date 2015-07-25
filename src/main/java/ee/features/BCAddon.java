8e0e20d4c5419970bcbaff460bd9455f2931851c
pomepome <punyoon03@gmail.com>
2015-07-25 16:13:21 +0900

    bug fixed

--- src/main/java/ee/features/BCAddon.java
+++ src/main/java/ee/features/BCAddon.java
@@ -1,11 +1,15 @@
 package ee.features;
 
+import buildcraft.BuildCraftCore;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
 public class BCAddon
 {
 	public static void load(EELimited mod)
 	{
-		//ItemStack woodEng = mod.gs(BuildCraftCore.engineBlock);
-		//ItemStack woodGear = mod.gs(BuildCraftCore.woodenGearItem);
-		//mod.addRecipe(mod.gs(mod.RFConverter),"GCG"," D ","GWG",'G',woodGear,'C',mod.EMCCollector,'D',Items.diamond);
+		ItemStack woodEng = mod.gs(BuildCraftCore.engineBlock);
+		ItemStack woodGear = mod.gs(BuildCraftCore.woodenGearItem);
+		mod.addRecipe(mod.gs(mod.RFConverter),"GCG"," D ","GWG",'G',woodGear,'C',mod.EMCCollector,'D',Items.diamond);
 	}
 }
