7b7775f6f65b4c87baa46aeb0fd811ef88c080e9
pomepome <punyoon03@gmail.com>
2015-11-29 02:07:00 +0900

    Beta 1.0.2.1

--- src/main/java/ee/features/items/ItemMiniumStone.java
+++ src/main/java/ee/features/items/ItemMiniumStone.java
@@ -1,15 +1,19 @@
 package ee.features.items;
 
-import ee.network.PacketHandler;
-import ee.network.PacketSound;
-import net.minecraft.item.ItemStack;
+import java.util.List;
 
-public class ItemMiniumStone extends ItemEEFunctional
+import ee.features.EELimited;
+import ee.features.items.interfaces.IExtraFunction;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class ItemMiniumStone extends ItemEEFunctional implements IExtraFunction
 {
 	public ItemMiniumStone()
 	{
 		super("MiniumStone");
-		this.setMaxDamage(20);
+		this.setMaxDamage(64);
 	}
 	@Override
     public ItemStack getContainerItem(ItemStack itemStack)
@@ -21,5 +25,13 @@ public class ItemMiniumStone extends ItemEEFunctional
 
         return copiedStack;
     }
-
+	@Override
+	public void onExtraFunction(EntityPlayer p, ItemStack is)
+	{
+		p.openGui(EELimited.instance, EELimited.CRAFT, p.worldObj, (int)p.posX, (int)p.posY, (int)p.posZ);
+	}
+	public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean par4)
+	{
+		list.add("Uses left:"+(this.getMaxDamage() - itemStack.getItemDamage()));
+	}
 }
